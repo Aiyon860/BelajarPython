dca12ce366f33a3742fd894993c8a3c54abd43cf
Daniel Adi Pratama <dapdapper14@gmail.com>
2023-09-19 12:04:04 +0700

    memodif file syntax.py dan menambah file variable.py

--- syntax.py
+++ syntax.py
@@ -4,8 +4,13 @@ if (5 > 2):
     print('A') #indent size must be in the same number of space, otherwise error.
 
 #python variable
-angka = 10
-nama = "Daniel Adi Pratama"
+angka = 10                      #initialization of angka
+nama = "Daniel Adi Pratama"     #initialization of nama
 print("1", nama)
 print(nama, angka)
 print("1", angka)
+
+"""
+this is multi-line comment
+test
+"""
---
+++ variable.py
@@ -0,0 +1,23 @@
+#python variable
+
+#1st ex
+crush = "Tirza"
+print(crush)
+
+#2nd ex
+kampus = "Polines"
+print(kampus)
+kampus = 1
+print(kampus)
+
+#3rd ex
+favourite_number1 = str(680)
+favourite_number2 = int(680)
+favourite_number3 = float(680)
+print()
+print()
+print()
+
+#4th ex
+
+
