4e66b2c3e6c6d17301805c9593d1bb984f1d7161
Daniel Adi Pratama <dapdapper14@gmail.com>
2023-09-21 05:45:58 +0700

    finish datatypes.py and add number.py

--- datatypes.py
+++ datatypes.py
@@ -5,77 +5,153 @@
 def FreeSpace():
     print(" ")
 
-a = "Halo, nama saya Daniel dan saya ganteng dan berintelektual" # string
-print(a)
-print(type(a))
-FreeSpace()
+def firstEx():
+    a = "Halo, nama saya Daniel dan saya ganteng dan berintelektual" # string
+    print(a)
+    print(type(a))
+    FreeSpace()
 
-b = 10                                                           # int
-print(b)
-print(type(b))
-FreeSpace()
+    b = 10                                                           # int
+    print(b)
+    print(type(b))
+    FreeSpace()
 
-c = 10.8                                                         # float
-print(c)
-print(type(c))
-FreeSpace()
+    c = 10.8                                                         # float
+    print(c)
+    print(type(c))
+    FreeSpace()
 
-d = 55J                                                          # complex
-print(d)
-print(type(d))
-FreeSpace()
+    d = 55J                                                          # complex
+    print(d)
+    print(type(d))
+    FreeSpace()
 
-e = ["McDonald", "KFC", "Hokben"]                                # list
-print(e)
-print(type(e))
-FreeSpace()
+    e = ["McDonald", "KFC", "Hokben"]                                # list
+    print(e)
+    print(type(e))
+    FreeSpace()
 
-f = ("McDonald", "KFC", "Hokben")                                # tuple
-print(f)
-print(type(f))
-FreeSpace()
+    f = ("McDonald", "KFC", "Hokben")                                # tuple
+    print(f)
+    print(type(f))
+    FreeSpace()
 
-g = range(2, 10)                                                    # range
-print(g)
-print(type(g))
-FreeSpace()
+    g = range(2, 10)                                                 # range
+    print(g)
+    print(type(g))
+    FreeSpace()
 
-h = {"nama" : "Daniel", "umur" : 18}                             # dict (like map, has key and value)
-print(h)
-print(type(h))
-FreeSpace()
+    h = {"nama" : "Daniel", "umur" : 18}                             # dict (like map, has key and value)
+    print(h)
+    print(type(h))
+    FreeSpace()
 
-i = {"McDonald", "KFC", "Hokben"}                                # set
-print(i)
-print(type(i))
-FreeSpace()
+    i = {"McDonald", "KFC", "Hokben"}                                # set
+    print(i)
+    print(type(i))
+    FreeSpace()
 
-j = frozenset({"McDonald", "KFC", "Hokben"})                     # frozenset
-print(j)
-print(type(j))
-FreeSpace()
+    j = frozenset({"McDonald", "KFC", "Hokben"})                     # frozenset
+    print(j)
+    print(type(j))
+    FreeSpace()
 
-k = True                                                         # bool
-print(k)
-print(type(k))
-FreeSpace()
+    k = True                                                         # bool
+    print(k)
+    print(type(k))
+    FreeSpace()
 
-l = b"Daniel Adi Pratama"                                        # bytes
-print(l)
-print(type(l))
-FreeSpace()
+    l = b"Daniel Adi Pratama"                                        # bytes
+    print(l)
+    print(type(l))
+    FreeSpace()
 
-m = bytearray(10)                                                # bytearray
-print(m)
-print(type(m))
-FreeSpace()
+    m = bytearray(10)                                                # bytearray
+    print(m)
+    print(type(m))
+    FreeSpace()
 
-n = memoryview(bytes(10))                                        # memoryview
-print(n)
-print(type(n))
-FreeSpace()
+    n = memoryview(bytes(10))                                        # memoryview
+    print(n)
+    print(type(n))
+    FreeSpace()
 
-o = None                                                         # NoneType
-print(o)
-print(type(o))
-FreeSpace()
+    o = None                                                         # NoneType
+    print(o)
+    print(type(o))
+    FreeSpace()
+
+## How to Specific directly to the data type value
+def secondEx():
+    satu = str("Halo saya Daniel")
+    print(satu)
+    print(type(satu))
+    FreeSpace()
+
+    dua = int(10)
+    print(dua)
+    print(type(dua))
+    FreeSpace()
+
+    tiga = float(15.6)
+    print(tiga)
+    print(type(tiga))
+    FreeSpace()
+
+    empat = complex(3+10J)
+    print(empat)
+    print(type(empat))
+    FreeSpace()
+
+    lima = list(("Badminton", "Basketball", "Volleyball"))
+    print(lima)
+    print(type(lima))
+    FreeSpace()
+
+    enam = tuple(("Badminton", "Basketball", "Volleyball"))
+    print(enam)
+    print(type(enam))
+    FreeSpace()
+
+    tujuh = range(2, 16, 4)
+    print(tujuh)
+    print(type(tujuh))
+    FreeSpace()
+
+    delapan = dict(Username="Daniel", PhoneNumber="0123456789")
+    print(delapan)
+    print(type(delapan))
+    FreeSpace()
+
+    sembilan = set(("Coca-Cola", "Fanta", "Sprite", "Pepsi"))
+    print(sembilan)
+    print(type(sembilan))
+    FreeSpace()
+
+    sepuluh = frozenset(("Coca-Cola", "Fanta", "Sprite", "Pepsi"))
+    print(sepuluh)
+    print(type(sepuluh))
+    FreeSpace()
+
+    sebelas = bool(-10)
+    print(sebelas)
+    print(type(sebelas))
+    FreeSpace()
+
+    duaBelas = bytes(20)
+    print(duaBelas)
+    print(type(duaBelas))
+    FreeSpace()
+
+    tigaBelas = bytearray(20)
+    print(tigaBelas)
+    print(type(tigaBelas))
+    FreeSpace()
+
+    empatbelas = memoryview(bytes(20))
+    print(empatbelas)
+    print(type(empatbelas))
+    FreeSpace()
+
+# For Testing
+secondEx()
---
+++ number.py
@@ -0,0 +1,71 @@
+### PYTHON NUNMBERS ###
+import random # for the last learning of this session (random number)
+
+def FreeSpace():
+    print('')
+    print("-----------------------------------------")
+    print('')
+
+## brief example:
+iniInt = int(50)        # int
+iniFloat = 31.98        # float
+iniComplex = 4+5j       # complex
+
+print(iniInt, "\t", type(iniInt))
+print(iniFloat, "\t", type(iniFloat))
+print(iniComplex, "\t", type(iniComplex))
+
+FreeSpace()
+
+## int
+int1 = 32
+int2 = 2043240237403274
+int3 = -3434
+
+print(int1, "\t\t\t", type(int1))
+print(int2, "\t", type(int2))
+print(int3, "\t\t\t", type(int3))
+
+FreeSpace()
+
+## float
+float1 = 34.56
+float2 = 1.2
+float3 = -33.778
+
+print(float1, "\t\t", type(float1))
+print(float2, "\t\t", type(float2))
+print(float3, "\t", type(float3))
+
+# e/E is scientific number that indicate the power of 10
+EFloat1 = 35e3 
+EFloat2 = 12E4
+EFloat3 = -67.3e10
+
+print('')
+
+print(EFloat1, "\t\t", type(EFloat1))
+print(EFloat2, "\t\t", type(EFloat2))
+print(EFloat3, "\t", type(EFloat3))
+
+FreeSpace()
+
+## complex, with j/J as the imaginary part
+complex1 = 6+23J
+complex2 = 9j
+complex3 = -145j
+
+print(complex1, "\t", type(complex1))
+print(complex2, "\t\t", type(complex2))
+print(complex3, "\t", type(complex3))
+
+FreeSpace()
+
+## TYPE CONVERSION
+A = int(140)        # int
+B = float(35.2)     # float
+C = complex(55j)    # complex
+
+
+
+## RANDOM NUMBER
